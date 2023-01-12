6e29e37b472e9ca99e173cd44686c112e5464b94
naveen kumar <121857854+navonwheels@users.noreply.github.com>
2023-01-12 18:18:32 +0530

    Add files via upload

---
+++ Holiday.java
@@ -0,0 +1,58 @@
+public class Holiday
+{
+	
+	private String name;
+	private int day;
+	private String month;
+		
+
+
+	public Holiday()
+{
+		 this.name =name;
+		 this.day =day;
+		 this.month =month;
+		
+}
+
+	public String getName() 
+{
+	return name;	
+}
+
+	public void setName(String name)
+{
+	this.name =name;
+}
+
+	public int getDay()
+{
+	return day;
+}
+
+	public void setDay(int day)
+{
+	this.day =day;
+}
+	public String getMonth() 
+{
+	return month;
+}
+	public void setMonth(String month)	
+{
+	this.month =month;
+}
+	public static void main(String args[])
+{
+	
+
+	Holiday call = new Holiday();
+	call.setName("instance");
+	call.setDay(14);
+	call.setMonth("july");
+	System.out.println(call.getName());
+	System.out.println(call.getDay());
+	System.out.println(call.getMonth());
+}
+}
+
